9f14096ce73c8215e6094de69825c5b28ee6d00a
Viktor Potoki <viktor.potoki@gmail.com>
2023-07-26 09:43:12 +0300

    added line Up

---
+++ 365WorldWide/365WorldWide/Screens/LineUpScreen/FormationCollectionViewCell.swift
@@ -0,0 +1,98 @@
+//
+//  FormationCollectionViewCell.swift
+//  365WorldWide
+//
+//  Created by MacBook on 25.07.2023.
+//
+
+import UIKit
+
+class FormationCollectionViewCell: UICollectionViewCell {
+    static let reuseIdentifier = String(describing: FormationCollectionViewCell.self)
+   
+    let bgImage = UIImageView()
+    let homeFormationImageView = UIImageView()
+    let awayFormationImageView = UIImageView()
+    let topConteinerView = UIView()
+    let homeTeamIconImageView = UIImageView()
+    let awayTeamIconImageView = UIImageView()
+    let formationLabelStack = LabelsStackView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+        setUpConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUp() {
+        bgImage.image = UIImage(named: "lineUpBG")
+        addSubview(bgImage)
+        addSubview(homeFormationImageView)
+        addSubview(awayFormationImageView)
+        addSubview(topConteinerView)
+        addSubview(homeTeamIconImageView)
+        addSubview(awayTeamIconImageView)
+        addSubview(formationLabelStack)
+        topConteinerView.backgroundColor = UIColor(red: 0.1686, green: 0.3529, blue: 0.2863, alpha: 0.5)
+        topConteinerView.layer.cornerRadius = 4
+        topConteinerView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        formationLabelStack.leftLabel.font = .systemFont(ofSize: 13, weight: .medium)
+        formationLabelStack.leftLabel.textColor = .mainYellowColor
+        formationLabelStack.rightLabel.font = .systemFont(ofSize: 13, weight: .medium)
+        formationLabelStack.rightLabel.textColor = .mainGreenColor
+        formationLabelStack.leftLabel.text = "4-4-4"
+        formationLabelStack.rightLabel.text = "44-4"
+
+    }
+    
+    private func setUpConstraints() {
+        bgImage.translatesAutoresizingMaskIntoConstraints = false
+        homeFormationImageView.translatesAutoresizingMaskIntoConstraints = false
+        awayFormationImageView.translatesAutoresizingMaskIntoConstraints = false
+        topConteinerView.translatesAutoresizingMaskIntoConstraints = false
+        formationLabelStack.translatesAutoresizingMaskIntoConstraints = false
+        homeTeamIconImageView.translatesAutoresizingMaskIntoConstraints = false
+        awayTeamIconImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            topConteinerView.topAnchor.constraint(equalTo: topAnchor, constant: 0),
+            topConteinerView.heightAnchor.constraint(equalToConstant: 44),
+            topConteinerView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            topConteinerView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0),
+            
+            homeTeamIconImageView.leadingAnchor.constraint(equalTo: topConteinerView.leadingAnchor, constant: 8),
+            homeTeamIconImageView.centerYAnchor.constraint(equalTo: topConteinerView.centerYAnchor, constant: 0),
+            homeTeamIconImageView.heightAnchor.constraint(equalToConstant: 20),
+            homeTeamIconImageView.widthAnchor.constraint(equalToConstant: 20),
+            
+            awayTeamIconImageView.trailingAnchor.constraint(equalTo: topConteinerView.trailingAnchor, constant: -8),
+            awayTeamIconImageView.centerYAnchor.constraint(equalTo: topConteinerView.centerYAnchor, constant: 0),
+            awayTeamIconImageView.heightAnchor.constraint(equalToConstant: 20),
+            awayTeamIconImageView.widthAnchor.constraint(equalToConstant: 20),
+            
+            formationLabelStack.centerYAnchor.constraint(equalTo: topConteinerView.centerYAnchor),
+            formationLabelStack.leadingAnchor.constraint(equalTo: homeTeamIconImageView.trailingAnchor, constant: 83),
+            formationLabelStack.trailingAnchor.constraint(equalTo: awayTeamIconImageView.leadingAnchor, constant: -83),
+            
+            bgImage.topAnchor.constraint(equalTo: topConteinerView.bottomAnchor),
+            bgImage.leadingAnchor.constraint(equalTo: leadingAnchor),
+            bgImage.trailingAnchor.constraint(equalTo: trailingAnchor),
+            bgImage.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            homeFormationImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            homeFormationImageView.widthAnchor.constraint(equalTo: bgImage.widthAnchor, multiplier: 0.49),
+            homeFormationImageView.topAnchor.constraint(equalTo: bgImage.topAnchor),
+            homeFormationImageView.bottomAnchor.constraint(equalTo: bgImage.bottomAnchor),
+            
+            awayFormationImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0),
+            awayFormationImageView.widthAnchor.constraint(equalTo: bgImage.widthAnchor, multiplier: 0.49),
+            awayFormationImageView.topAnchor.constraint(equalTo: bgImage.topAnchor),
+            awayFormationImageView.bottomAnchor.constraint(equalTo: bgImage.bottomAnchor),
+        ])
+    }
+}
---
+++ 365WorldWide/365WorldWide/Screens/LineUpScreen/LineUpView.swift
@@ -0,0 +1,106 @@
+//
+//  LineUpView.swift
+//  365WorldWide
+//
+//  Created by MacBook on 25.07.2023.
+//
+
+import UIKit
+
+class LineUpView: UIView {
+
+    var collectionView: UICollectionView!
+    
+    private let topBGView = TopDetailView()
+   
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+        setUpCollectionView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUp() {
+        self.backgroundColor = .mainBGColor
+        addSubview(topBGView)
+        topBGView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            topBGView.topAnchor.constraint(equalTo: topAnchor),
+            topBGView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            topBGView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            topBGView.heightAnchor.constraint(equalToConstant: 153),
+            
+        ])
+    }
+    
+    private func setUpCollectionView() {
+        collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: generateLayout())
+        self.addSubview(collectionView)
+        collectionView.backgroundColor = .clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(LineUpCollectionViewCell.self, forCellWithReuseIdentifier: LineUpCollectionViewCell.reuseIdentifier)
+        collectionView.register(FormationCollectionViewCell.self, forCellWithReuseIdentifier: FormationCollectionViewCell.reuseIdentifier)
+        collectionView.register(LineUpHeaderCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: LineUpHeaderCollectionReusableView.reuseIdentifier)
+        
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            collectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            collectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            collectionView.topAnchor.constraint(equalTo: topBGView.bottomAnchor, constant: 24)
+        ])
+    }
+    
+    private func generateLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout (sectionProvider: { (sectionIndex: Int,
+                                                                              layoutEnvironment: NSCollectionLayoutEnvironment)
+            -> NSCollectionLayoutSection? in
+            if sectionIndex == 0 {
+                return self.generateLayoutFirstSection()
+            } else {
+                return self.generateLayoutSection()
+            }
+        })
+        return layout
+    }
+    
+    private func generateLayoutSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .fractionalHeight(1))
+        let fullSizeItem = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(28))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [fullSizeItem])
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .none
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                    heightDimension: .absolute(52))
+            let header = NSCollectionLayoutBoundarySupplementaryItem(
+                        layoutSize: headerSize,
+                        elementKind: UICollectionView.elementKindSectionHeader,
+                        alignment: .top)
+        section.boundarySupplementaryItems = [header]
+        return section
+    }
+    
+    private func generateLayoutFirstSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .fractionalHeight(1))
+        let fullSizeItem = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(281))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [fullSizeItem])
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .none
+        return section
+    }
+
+}
